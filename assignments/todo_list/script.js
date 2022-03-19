5fc34d74088c506e82483ccc5ee445d88b9df75a
Your Name <you@example.com>
2022-03-19 11:37:55 +0530

    assignment 7 completed

---
+++ assignments/todo_list/script.js
@@ -0,0 +1,17 @@
+let input=document.querySelector('input')
+let btn=document.querySelector('button')
+let todos=document.querySelector('.todos')
+
+let count=-1
+btn.addEventListener('click',function(){
+    count+=1
+    let todo=document.createElement('p')
+    todo.innerHTML=input.value
+    todo.style.cursor='pointer'
+    todo.setAttribute("key",count)
+    todos.appendChild(todo)
+    todo.addEventListener('click',function(){
+        todos.removeChild(todo)
+    })
+})
+
